f0c39eaff1a95bffd5ae21c8fbd6cdf2cff3224f
Arhip Storozhev <49954420+arsestor@users.noreply.github.com>
2020-11-28 11:33:27 +0500

    Finish python file
    Added comments

--- image-enlarger.py
+++ image-enlarger.py
@@ -3,24 +3,30 @@ image = Image.open(r"E:\Files\file.png")
 
 multiplier = int(input())
 
-size = image.size
+# Collect data from a given image
+size = image.size  # We get tuple (width, height)
 new_size = image.size[0] * multiplier, image.size[1] * multiplier
 mode = image.mode
 format = image.format
 
+# Get pixel colors list of for a given image...
 pixel_colors = list(image.getdata())
+# ... and divide it into "height" equal parts
 pixel_colors = [pixel_colors[i:i+size[0]] for i in range(0, len(pixel_colors), size[0])]
 
+# Create a pixel colors list for a new image
 new_pixel_colors = []
 for i in pixel_colors:
     for j in range(multiplier):
         [new_pixel_colors.append(val) for val in i for _ in range(multiplier)]
 
+# Create a coordinate list for a new image
 coordinates = []
 for y in range(0,new_size[1]):
     for x in range(0,new_size[0]):
         coordinates.append((x,y))
 
+# Create new image and save it
 img = Image.new("RGB", new_size, (255,255,255))
 draw = ImageDraw.Draw(img)
 for i in range(new_size[0]*new_size[1]):
